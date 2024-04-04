16e47149351e932b36c5e13b072aa094fdb1828c
that30sCoder <rahulksingh1092@gmail.com>
2024-04-05 03:55:55 +0530

    Initial commit

---
+++ src/main/java/com/that30sCoder/springJpaDemo/SpringJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.that30sCoder.springJpaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/that30sCoder/springJpaDemo/SpringJpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.that30sCoder.springJpaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

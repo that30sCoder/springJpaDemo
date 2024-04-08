88f06ab9627d03675dde8de7c5287a5c064ecb63
that30sCoder <rahulksingh1092@gmail.com>
2024-04-09 03:19:40 +0530

    Simple Tests

--- src/test/java/com/that30sCoder/springJpaDemo/SpringJpaDemoApplicationTests.java
+++ src/test/java/com/that30sCoder/springJpaDemo/SpringJpaDemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.that30sCoder.springJpaDemo;
 
+import com.that30sCoder.springJpaDemo.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class SpringJpaDemoApplicationTests {
 
+	@Autowired
+	BookRepository bookRepository;
+
+
+	@Test
+	void testBookRepository(){
+		long count = bookRepository.count();
+
+		assertThat(count).isGreaterThan(0);
+	}
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
